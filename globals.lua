0cb3de8cfb59a35d38247a80b782a787be32f608
VioletShard2690 <romachizhiov@gmail.com>
2026-05-15 21:33:55 +0700

    first mod version created

--- globals.lua
+++ globals.lua
@@ -1,4 +1,6 @@
-G.C.uv = {
+--- GLOBALS
+
+G.C.SJ = {
     RED = HEX("FF0000"),
     BLACK = HEX("000000"),
     BLUE = HEX("0000FF"),
@@ -6,16 +8,19 @@ G.C.uv = {
     WHITE = HEX("FFFFFF"),
     TRANSPARENT = HEX("00000000"),
 }
+
+-- Hooks
+
 local loc_colour_ref = loc_colour
 function loc_colour(_c, _default)
     if not G.ARGS.LOC_COLOURS then
         loc_colour_ref()
     end
-    G.ARGS.LOC_COLOURS._red = G.C.uv.RED
-    G.ARGS.LOC_COLOURS._black = G.C.uv.BLACK
-    G.ARGS.LOC_COLOURS._blue = G.C.uv.BLUE
-    G.ARGS.LOC_COLOURS._green = G.C.uv.GREEN
-    G.ARGS.LOC_COLOURS._white = G.C.uv.WHITE
-    G.ARGS.LOC_COLOURS._transparent = G.C.uv.TRANSPARENT
+    G.ARGS.LOC_COLOURS.sj_red = G.C.SJ.RED
+    G.ARGS.LOC_COLOURS.sj_black = G.C.SJ.BLACK
+    G.ARGS.LOC_COLOURS.sj_blue = G.C.SJ.BLUE
+    G.ARGS.LOC_COLOURS.sj_green = G.C.SJ.GREEN
+    G.ARGS.LOC_COLOURS.sj_white = G.C.SJ.WHITE
+    G.ARGS.LOC_COLOURS.sj_transparent = G.C.SJ.TRANSPARENT
     return loc_colour_ref(_c, _default)
 end
